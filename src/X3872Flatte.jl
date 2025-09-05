a71b608e6271990d03015594680440bf18ba1ed2
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2025-09-05 10:45:53 +0200

    remove export for e2m, m2e and branching methods

--- src/X3872Flatte.jl
+++ src/X3872Flatte.jl
@@ -6,8 +6,6 @@ using NLsolve
 using QuadGK
 using Optim
 
-export e2m, m2e
-export fm_times_GeV
 include("masseswidths.jl")
 
 export FlatteModel
@@ -18,8 +16,6 @@ export scattering_parameters
 export pole_position
 include("flatte.jl")
 
-export BXρ, BXω
-export dRρ, dRω, dRDˣ⁰D⁰, dR0
 include("branchings.jl")
 
 include("plausible_parameters.jl")
